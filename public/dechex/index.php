daa6db1b691978aac18770b232f29163c76a6934
Villermen <villermen@gmail.com>
2023-07-16 17:01:42 +0200

    Give DECHEX some basic bootstrap flavor

--- public/dechex/index.php
+++ public/dechex/index.php
@@ -9,35 +9,52 @@ $app = new \Villermen\Toolbox\App();
     'script' => $app->createPath('dechex/script.js'),
 ]); ?>
 <form>
-    <input name="query" type="text" />
-    <p>From</p>
-    <label>
-        <input name="system" type="radio" value="dec" />
-        Decimal
-    </label><br />
-    <label>
-        <input name="system" type="radio" value="hex" />
-        Hexadecimal
-    </label><br />
-    <label>
-        <input name="system" type="radio" value="bin" />
-        Binary
-    </label><br />
-    <p>Endianness (out)</p>
-    <label>
-        <input name="endianness" type="radio" value="big" />
-        Big-endian (BE, significant-first)
-    </label><br />
-    <label>
-        <input name="endianness" type="radio" value="little" />
-        Little-endian (LE, significant-last)
-    </label>
+    <div class="mb-3">
+        <label class="form-label" for="queryInput">Convert</label>
+        <input class="form-control" name="query" type="text" id="queryInput" />
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="systemRadioDec">From</label>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="system" value="dec" id="systemRadioDec" checked>
+            <label class="form-check-label" for="systemRadioDec">
+                Decimal
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="system" value="hex" id="systemRadioHex">
+            <label class="form-check-label" for="systemRadioHex">
+                Hexadecimal
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="system" value="bin" id="systemRadioBin">
+            <label class="form-check-label" for="systemRadioBin">
+                Binary
+            </label>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label class="form-label" for="endianRadioBig">Endianness (out)</label>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="endianness" value="big" id="endianRadioBig" checked>
+            <label class="form-check-label" for="endianRadioBig">
+                Big-endian (BE, significant-first)
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="endianness" value="little" id="endianRadioLittle" checked>
+            <label class="form-check-label" for="endianRadioLittle">
+                Little-endian (LE, significant-last)
+            </label>
+        </div>
+    </div>
 </form>
-<p>Result</p>
-<table>
+<p><strong>Result</strong></p>
+<table class="table">
     <tbody>
         <tr>
-            <td>Decimal</td>
+            <td style="width: 170px;">Decimal</td>
             <td id="resultDec"></td>
             <td id="resultDecFormatted"></td>
         </tr>
