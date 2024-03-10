b3e9c29abdb576a7f348d075ba3c8b7a9a7a567f
Villermen <villermen@gmail.com>
2024-03-10 19:51:04 +0100

    Fix URL to JS

--- public/work/index.php
+++ public/work/index.php
@@ -151,7 +151,7 @@ if ($profile) {
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"
     ></script>',
-    'script' => $app->createPath('work/index.js'),
+    'script' => $app->createPath('work/work.js'),
 ]); ?>
 <?php if ($profile): ?>
     <form method="get" action="<?= $app->createUrl('work/form.php'); ?>">
